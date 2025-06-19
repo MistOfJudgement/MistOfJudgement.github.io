0c53b91e38bdf55e0e7497643dedc58417c2374a
Tushar <45638547+MistOfJudgement@users.noreply.github.com>
2025-06-18 20:59:21 -0400

    setup typescript

---
+++ src/architect.ts
@@ -0,0 +1,6 @@
+import { buildPage } from "./pages/index.blueprint";
+
+console.log("Architect is running...");
+const index = buildPage();
+console.log(index);
+console.log("Architect has finished running.");
---
+++ src/pages/index.blueprint.ts
@@ -0,0 +1,4 @@
+
+export function buildPage() : string {
+    return "<!DOCTYPE html>"
+}
